c242e6220b6a3c690ff05d086bf06821c341c273
Initred <initred@gmail.com>
2026-09-09 16:10:37 +0900

    chore(artisan): serve 개발 명령 제외
    Sail을 사용하므로 `php artisan serve`를 DevCommands에서 제외한다.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Carbon\CarbonImmutable;
+use Illuminate\Foundation\DevCommands;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
@@ -25,6 +26,7 @@ final class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        DevCommands::except('server');
         $this->configureDefaults();
     }
 
